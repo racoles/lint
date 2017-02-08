03852900c9b937dcf3c38407cfb73d48e3d6e95b
shouraku <shouraku@gmail.com>
2017-02-08 14:15:52 -0500

    Auto move output products to output products file

--- loadFITS.py
+++ loadFITS.py
@@ -13,11 +13,16 @@ from loadConfig import loadConfig
 
 def outputsFolder(files_path, *default_parameters, **keyword_parameters):
 #make a directory for output products and move output products into it
+#Optional parameters: 
+#    'newFolder': indicates that you want to create a new output product folder (in the same directory as the fits files folder)
+#    'moveFileName' = filename:  indicates that you want to move the named file into the output product folder
+#Note: if you are making a new dir (newFolder): files_path is the fits files location
+#      if you are moving files to the output folder (moveFileName): files_path is the location of the output folder
     #Create folder for output products named with the date
     if ('newFolder' in keyword_parameters):
         today = datetime.date.today()  # get today's date as a datetime type  
         todaystr = today.isoformat()   # get string representation: YYYY-MM-DD from a datetime type.
-        output_path = os.path.dirname(os.path.dirname(files_path))
+        output_path = os.path.dirname(os.path.dirname(files_path)) #Here files_path is fits files location
         #Make output dir. Append # to dir name if dir already exists
         if not os.path.exists(os.path.join(output_path, todaystr)):  #Folder doesn't exist
             new_folder_path = os.mkdir(os.path.join(output_path, todaystr))
@@ -30,9 +35,10 @@ def outputsFolder(files_path, *default_parameters, **keyword_parameters):
                    folder_exist = False
                    iterator = iterator + 1
                 return new_folder_path
-    elif ('moveFileName' in keyword_parameters): #just moving files, not making a dir
+    #Just moving files, not making a dir. Here, files_path is where you want to move the files to
+    elif ('moveFileName' in keyword_parameters):
         if os.path.isfile(os.path.join(os.getcwd(), keyword_parameters['moveFileName'])):
-            os.rename(os.path.join(os.getcwd(), keyword_parameters['moveFileName']), os.path.join(new_path, keyword_parameters['moveFileName']))
+            os.rename(os.path.join(os.getcwd(), keyword_parameters['moveFileName']), os.path.join(files_path, keyword_parameters['moveFileName']))
 
 def makeList(fileDir):
 #make a list of the files in the directory
