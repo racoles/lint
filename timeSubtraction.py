cbaeae96ce3e886c73f4bfe3d5cf7bacf75c514b
shouraku <shouraku@gmail.com>
2017-02-13 14:22:02 -0500

    Fixed overscan subtraction

--- timeSubtraction.py
+++ timeSubtraction.py
@@ -14,9 +14,11 @@ def timeSub(fileDir, ext):
     #load the fits file image data into a list (3D array)
     filepathsAndFileNames = makeList(fileDir)     
     files = openFiles(filepathsAndFileNames, ext)
-    #Convert to 3D numpy array
+    #convert to 3D numpy array
     numpyArray = array(files)
     #subtract the last file from all of the preceding ones
+    timeSubtracted = numpyArray - numpyArray[numpyArray.shape[0]-1,:,:]
+    print timeSubtracted[timeSubtracted.shape[0]-1,:,:]
     #delete the fits that was used for the subtraction
-    #return delete(files-files[-1], [len(filepathsAndFileNames)-1], 0)
+
     return numpyArray
