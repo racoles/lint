078705eb6a96d1ede8bb3113cac6116838effe30
shouraku <shouraku@gmail.com>
2017-02-27 14:35:22 -0500

    Creating batch processing

--- batch.py
+++ batch.py
@@ -56,30 +56,33 @@ def processByDate(lintDict):
     #group fits files by date
     dateList, groups = autoGroup(loadFITS.makeList(lintDict['fitsPath']), lintDict['ext'])
     #Processing loop
-    groupNumber = 0 #this will track which group is being processed by lint
+    groupNumber = 0 #this will track which group is being processed by LINT
+    #loop start
+    for ii in range(groups):
     #Extract list of objects in a given group from dateList
+    
     #Process group
 ######
-    #Subtract overscan, and mask overscan regions, if overscanSubtractBOOL is "True"
-    fitsArrayOverscanSubtracted = subtractOverscan(lintDict['overscanSubtractBOOL'], lintDict['overscanRows'],
-                                                   lintDict['overscanColumns'], loadFITS.openFiles(loadFITS.makeList(lintDict['fitsPath']), lintDict['ext'], lintDict['rows'], lintDict['columns']))
-    #prepare the image for analysis
-        #scale and stack images.
-        #subtract the average sky value from the image. 
+        #Subtract overscan, and mask overscan regions, if overscanSubtractBOOL is "True"
+        fitsArrayOverscanSubtracted = subtractOverscan(lintDict['overscanSubtractBOOL'], lintDict['overscanRows'],
+                                                       lintDict['overscanColumns'], loadFITS.openFiles(loadFITS.makeList(lintDict['fitsPath']), lintDict['ext'], lintDict['rows'], lintDict['columns']))
+        #prepare the image for analysis
+            #scale and stack images.
+            #subtract the average sky value from the image. 
         #invert the image to make attenuation spots appear positive to photometry code.
-    invertedImage = (subtractAverageSky(scaleAndStack.stackImages(scaleAndStack.scaleToMean(fitsArrayOverscanSubtracted))))*(-1)
-    #save the scaled, stacked, inverted image
-    outputName, output_folder_path = loadFITS.saveFITS(lintDict['fitsPath'], invertedImage, lintDict['outputFITS'])
-    #send the scaled/stacked/inverted image to SExtractor with the user give parameter file
-    callSExtractor.sendSEImage(lintDict['fitsPath'], outputName, lintDict['SExParameterFile'])
-    #read SExtractor output file, and make cuts to SExtractor output table
-    cutTable = analyzeSExOutput.cutsSExOutput(analyzeSExOutput.loadSExOutput(lintDict['SExParameterFile'], output_folder_path),
-                                              lintDict['flagLimit'], lintDict['fluxLimit'], lintDict['SNRlimit'], 
-                                              lintDict['FWHMlimit'], output_folder_path)
-    #create the histogram
-    n1, bin_centers = analyzeSExOutput.createHist(cutTable, output_folder_path)
-    #create a log10 plot
-    logNPlot = analyzeSExOutput.logPlot(n1, bin_centers, output_folder_path)
+        invertedImage = (subtractAverageSky(scaleAndStack.stackImages(scaleAndStack.scaleToMean(fitsArrayOverscanSubtracted))))*(-1)
+        #save the scaled, stacked, inverted image
+        outputName, output_folder_path = loadFITS.saveFITS(lintDict['fitsPath'], invertedImage, lintDict['outputFITS'])
+        #send the scaled/stacked/inverted image to SExtractor with the user give parameter file
+        callSExtractor.sendSEImage(lintDict['fitsPath'], outputName, lintDict['SExParameterFile'])
+        #read SExtractor output file, and make cuts to SExtractor output table
+        cutTable = analyzeSExOutput.cutsSExOutput(analyzeSExOutput.loadSExOutput(lintDict['SExParameterFile'], output_folder_path),
+                                                  lintDict['flagLimit'], lintDict['fluxLimit'], lintDict['SNRlimit'], 
+                                                  lintDict['FWHMlimit'], output_folder_path)
+        #create the histogram
+        n1, bin_centers = analyzeSExOutput.createHist(cutTable, output_folder_path)
+        #create a log10 plot
+        logNPlot = analyzeSExOutput.logPlot(n1, bin_centers, output_folder_path)
 
 def timePlot(datelist):
 #Plot the debris accumulation over time as: hist
